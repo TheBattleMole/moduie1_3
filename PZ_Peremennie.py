ceb1a02e40856d3bf03b5f79b036253fc588f2b4
Aleksey_Abramenko <x903xxxx@gmail.com>
2024-11-17 13:29:42 +0300

    Add files via upload

---
+++ PZ_Peremennie.py
@@ -0,0 +1,5 @@
+Kolichestvo_DZ = 12
+Zatracheno_chasov = 1.5
+Nazvanie_kyrsa = "Python"
+Vremya_na_oodno_zadanie = (Zatracheno_chasov/Kolichestvo_DZ)
+print("Курс:",Nazvanie_kyrsa,"всего задач:", Kolichestvo_DZ,"затрачено часов:",Zatracheno_chasov,"среднее время выполнения",Vremya_na_oodno_zadanie)
